059df46cbdec19f7946503936a8032393402a8c2
Mako Bates <33739499+ShapeOfMatter@users.noreply.github.com>
2021-04-18 21:17:24 -0400

    move stuff around

--- samp_utils.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def true_subgraph(G, nodes_to_keep):
-    G_sub = G.copy(as_view=False)
-    G_sub.remove_nodes_from([n for n in G if n not in set(nodes_to_keep)])
-    return G_sub
-
-
--- train_gcn/dgldataset.py
+++ train_gcn/dgldataset.py
@@ -2,10 +2,14 @@ import dgl
 from dgl.data import DGLDataset
 import torch
 from typing import Sequence
-from samp_utils import true_subgraph
 
 from tqdm import tqdm
 import networkx as nx
+
+def true_subgraph(G, nodes_to_keep):
+    G_sub = G.copy(as_view=False)
+    G_sub.remove_nodes_from([n for n in G if n not in set(nodes_to_keep)])
+    return G_sub
 
 class SyntheticDataset(DGLDataset):
     def __init__(self, indices, master_dir, sub_graph_choices):
